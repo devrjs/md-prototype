3dd9ea5cfb02758d490b2dcee60d522b850c0800
Vitor Gomes <vitorjgomes25@gmail.com>
2025-04-27 18:00:15 -0300

    build: routes

---
+++ src/middleware.ts
@@ -0,0 +1,59 @@
+import {
+  type MiddlewareConfig,
+  type NextRequest,
+  NextResponse,
+} from 'next/server'
+
+const publicRoutes = [
+  { path: '/', whenAuthenticated: 'redirect' },
+  { path: '/sign-in', whenAuthenticated: 'redirect' },
+  { path: '/register', whenAuthenticated: 'redirect' },
+  { path: '/pricing', whenAuthenticated: 'next' },
+] as const
+
+const REDIRECT_WHEN_AUTHENTICATED_ROUTE = '/sign-in'
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+  const publicRoute = publicRoutes.find(route => route.path === path)
+  const authToken = request.cookies.get('access_token')
+
+  if (!authToken && publicRoute) {
+    return NextResponse.next()
+  }
+
+  if (!authToken && !publicRoute) {
+    const redirectUrl = request.nextUrl.clone()
+
+    redirectUrl.pathname = REDIRECT_WHEN_AUTHENTICATED_ROUTE
+
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  if (
+    authToken &&
+    publicRoute &&
+    publicRoute.whenAuthenticated === 'redirect'
+  ) {
+    const redirectUrl = request.nextUrl.clone()
+
+    redirectUrl.pathname = '/dashboard'
+
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  if (authToken && !publicRoute) {
+    // Checar se o jwt não está expirado
+    // Se estiver expirado, remover o cookie e redirecionar para a página de login
+
+    return NextResponse.next()
+  }
+
+  return NextResponse.next()
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
