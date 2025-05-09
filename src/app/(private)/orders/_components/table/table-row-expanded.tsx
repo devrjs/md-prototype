4bca64439df2334565226bfb804dae143bfcb69b
Vitor Gomes <vitorjgomes25@gmail.com>
2025-05-09 08:51:01 -0300

    feat: table row expanded

--- src/app/(private)/orders/_components/table/table-row-expanded.tsx
+++ src/app/(private)/orders/_components/table/table-row-expanded.tsx
@@ -22,14 +22,57 @@ export function TableRowExpanded({ row }: TableRowExpandedProps) {
         colSpan={row.getVisibleCells().length}
         className="p-4 bg-muted/30"
       >
-        <div className="flex items-start py-2">
-          <div className="ml-7 flex flex-col gap-2 w-full">
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 text-sm">
-              <div>
-                <span className="font-medium">Número do Pedido:</span>{' '}
-                {orderData?.order_payment?.buyer_payment_method}
+        <div className="flex gap-6">
+          <div className="flex flex-col gap-6">
+            {orderData?.items.map(item => (
+              <div key={item.id} className="flex flex-col">
+                <span>order_id: {item.order_id}</span>
+                <span>item_id: {item.id}</span>
+                <span>name: {item.name}</span>
+                <span>quantity: {item.quantity}</span>
+                <span>unit_price: {item.unit_price}</span>
+                <span>
+                  unit_price_with_discount: {item.unit_price_with_discount}
+                </span>
+                <span>variation_sku: {item.variation_sku}</span>
               </div>
-            </div>
+            ))}
+          </div>
+
+          <div className="flex flex-col">
+            <span>
+              amount_received_from_sale:{' '}
+              {Number(orderData?.order_payment?.amount_received_from_sale)}
+            </span>
+            <span>
+              buyer_paid_amount:{' '}
+              {Number(orderData?.order_payment?.buyer_paid_amount)}
+            </span>
+            <span>
+              platform_coins: {Number(orderData?.order_payment?.platform_coins)}
+            </span>
+            <span>
+              platform_discount:{' '}
+              {Number(orderData?.order_payment?.platform_discount)}
+            </span>
+            <span>
+              seller_discount:{' '}
+              {Number(orderData?.order_payment?.seller_discount)}
+            </span>
+            <span>
+              total_order_price:{' '}
+              {Number(orderData?.order_payment?.total_order_price)}
+            </span>
+            <span>
+              total_platform_fee:{' '}
+              {Number(orderData?.order_payment?.total_platform_fee)}
+            </span>
+            <span>
+              total_platform_fee_with_discounts:{' '}
+              {Number(
+                orderData?.order_payment?.total_platform_fee_with_discounts
+              )}
+            </span>
           </div>
         </div>
       </TableCell>
